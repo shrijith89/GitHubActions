2e5d9ff46ea005a9915aaf5ab194d2c887931c40
shrijith89 <p.shrijith@talview.com>
2022-11-06 12:37:15 +0530

    jenkinsProject

---
+++ src/test/java/com/demo/Demo.java
@@ -0,0 +1,25 @@
+package com.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+	
+	static WebDriver driver;
+
+	@BeforeTest
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void demo() {
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+	}
+}
